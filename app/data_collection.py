5d6b9fc4717b26bc2f5e07f0fddd6ef315a6ad4a
gundralaa <mathgame99@gmail.com>
2017-09-09 21:57:20 -0700

    Data collection working

--- app/data_collection.py
+++ app/data_collection.py
@@ -2,24 +2,30 @@ import sqlite3
 
 import Adafruit_ADS1x15
 
+var = 1
 
 def log_values(ecg):
-    conn = sqlite3.connect('projects/heart_attack_detection/lab_app.db')  # It is important to provide an
+    conn = sqlite3.connect('ecg_data.db')  # It is important to provide an
     # absolute path to the database
     # file, otherwise Cron won't be
     # able to find it!
     curs = conn.cursor()
-    curs.execute("""INSERT INTO ECG values((datetime(CURRENT_TIMESTAMP, 'localtime'), (?))""", ecg)
+    print(ecg)
+    print(type(ecg))
+    curs.execute("insert into ECG values(datetime(CURRENT_TIMESTAMP,'localtime'), (?))", (ecg,))
     conn.commit()
     conn.close()
 
-adc = Adafruit_ADS1x15.ADS1015()
-ecg_data = adc.read_adc(1, data_rate=256)
 
-if ecg_data is not None:
-    log_values(ecg_data)
-else:
-    log_values(1024)
+while(var == 1):
+
+    adc = Adafruit_ADS1x15.ADS1015()
+    ecg_data = adc.read_adc(1, data_rate=250)
+
+    if ecg_data is not None:
+       log_values(ecg_data)
+    else:
+       log_values(1024)
 
 
 
