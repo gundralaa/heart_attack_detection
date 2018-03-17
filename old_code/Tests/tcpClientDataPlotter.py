7ca6e7431a549959583e6205536828d302585542
root <root@localhost.localdomain>
2018-03-17 12:22:54 -0700

    Clietn Changes

--- old_code/Tests/tcpClientDataPlotter.py
+++ old_code/Tests/tcpClientDataPlotter.py
@@ -5,8 +5,8 @@ from matplotlib import style
 import biosppy.signals.tools as st
 import numpy as np
 
-TCP_IP = '10.0.0.68'
-TCP_PORT = 5005
+TCP_IP = '10.0.0.69'
+TCP_PORT = 5001
 BUFFER_SIZE = 1024
 CON_MESSAGE = "Connected"
 
@@ -21,9 +21,13 @@ s.connect((TCP_IP,TCP_PORT))
 s.send(CON_MESSAGE)
 data = []
 while True:
-	point = s.recv(BUFFER_SIZE)
-	if not data: break
-	np.append[data, point]
-	filtered, _, _ = st.filter_signal(signal=data,ftype='FIR', band='bandpass', order= int(0.3 * 256), frequency=[3,45],sampling_rate=256)
-	np.savetxt('', filtered)
+	while 1:
+		point = s.recv(BUFFER_SIZE)
+		if not point: break
+		print "Status: ", point
+	if not point: pass
+	np.append(data, point)
+
+#filtered, _, _ = st.filter_signal(signal=data,ftype='FIR', band='bandpass', order= int(0.3 * 256), frequency=[3,45],sampling_rate=256)
+np.savetxt('', filtered)
 s.close()
